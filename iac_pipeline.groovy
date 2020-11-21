86844e07e74d379a09e669e03559f46e45911f66
AZFAR HOSSAIN <ahossain@us.ibm.com>
2020-11-21 17:52:46 -0500

    My first commit to this repository 11/21 5:52 pm

--- iac_pipeline.groovy
+++ iac_pipeline.groovy
@@ -5,7 +5,7 @@ pipeline {
     stage('Submit Stack') { 
       steps {
         //sh "aws cloudformation create-stack --stack-name myteststack --template-body file:///home/testuser/mytemplate.json --region 'us-east-1' --parameters ParameterKey=Parm1,ParameterValue=test1 ParameterKey=Parm2,ParameterValue=test2"
-         sh "aws cloudformation create-stack --stack-name sundayWebServer --template-body  s3://cf-templates-a1jqt245fpux-us-east-1/TrainingEvent-BasicWebServer.json --region 'us-east-1' --parameters ParameterKey=InstanceType,ParameterValue='t2.micro' ParameterKey=KeyName,ParameterValue='s3://cf-templates-a1jqt245fpux-us-east-1/DevOpsKeyPair.pem' ParameterKey=SSHLocation,ParameterValue='0.0.0.0/0'"
+         sh "aws cloudformation create-stack --stack-name sundayWebServer --template-body  cloudformation/TrainingEvent-BasicWebServer.json --region 'us-east-1' --parameters ParameterKey=InstanceType,ParameterValue='t2.micro' ParameterKey=KeyName,ParameterValue='s3://cf-templates-a1jqt245fpux-us-east-1/DevOpsKeyPair.pem' ParameterKey=SSHLocation,ParameterValue='0.0.0.0/0'"
       }
     }
   }
