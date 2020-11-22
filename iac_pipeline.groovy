5efb9cf7cc028f0040f6eb9eba07ac64a6573f9d
AZFAR HOSSAIN <ahossain@us.ibm.com>
2020-11-21 22:40:20 -0500

    My first commit to this repository 11/21 10:40 pm

--- iac_pipeline.groovy
+++ iac_pipeline.groovy
@@ -7,7 +7,7 @@ pipeline {
         //sh "aws cloudformation create-stack --stack-name myteststack --template-body file:///home/testuser/mytemplate.json --region 'us-east-1' --parameters ParameterKey=Parm1,ParameterValue=test1 ParameterKey=Parm2,ParameterValue=test2"
         //sh "aws cloudformation create-stack --stack-name sundayWebStack --template-file  $workspace/cloudformation/sunday_web_stack.template --region 'us-east-1' --parameters ParameterKey=InstanceType,ParameterValue=t2.micro ParameterKey=KeyName,ParameterValue='s3://cf-templates-a1jqt245fpux-us-east-1/DevOpsKeyPair.pem' ParameterKey=SSHLocation,ParameterValue=0.0.0.0/0"
         //sh "aws cloudformation validate-template --region 'us-east-1' --template-file  $workspace/cloudformation/sunday_web_stack.json" 
-        sh "aws cloudformation deploy --stack-name sundayWebStack --template-file  $workspace/cloudformation/sunday_web_stack.template --region 'us-east-1' --capabilities CAPABILITY_IAM --parameters ParameterKey=InstanceType,ParameterValue=t2.micro ParameterKey=KeyName,ParameterValue='s3://cf-templates-a1jqt245fpux-us-east-1/DevOpsKeyPair.pem' ParameterKey=SSHLocation,ParameterValue=0.0.0.0/0"
+        sh "aws cloudformation deploy --template-file  '$workspace/cloudformation/sunday_web_stack.template' --stack-name sundayWebStack --region 'us-east-1' --parameter-overrides InstanceType=t2.micro KeyName='s3://cf-templates-a1jqt245fpux-us-east-1/DevOpsKeyPair.pem' SSHLocation=0.0.0.0/0"
       }
     }
   }
