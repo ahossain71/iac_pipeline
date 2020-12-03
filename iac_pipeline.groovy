f51ff648e218577571c15fc34dcbe8380fca0206
AZFAR HOSSAIN <ahossain@us.ibm.com>
2020-12-03 00:56:50 -0500

    Changed the image type to reolace aws linux to ubuntu to be bale to install tomcat 12/3 12:56 am

--- iac_pipeline.groovy
+++ iac_pipeline.groovy
@@ -17,9 +17,6 @@ pipeline {
           withCredentials([sshUserPrivateKey(credentialsId: 'cff1d3fe-236f-43ca-8ff5-5f37ec63422d', keyFileVariable: 'myKEY')]) {
              sh 'ansible-playbook ./ansible/playbooks/tomcat-setup.yml --user ubuntu --key-file ${myKEY}'  
            }//end withCredentials
-         
-         
-         }//end with
           sh "exit 1"
          }//end catchError
       }//end steps
