8f89ab182338bb901cb1c003152fc16eefc9653b
AZFAR HOSSAIN <ahossain@us.ibm.com>
2020-12-02 23:32:24 -0500

    Changed the image type to reolace aws linux to ubuntu to be bale to install tomcat 12/2 11:32 pm

--- iac_pipeline.groovy
+++ iac_pipeline.groovy
@@ -7,8 +7,8 @@ pipeline {
         // sh "aws cloudformation create-stack --stack-name myteststack --template-body file:///home/testuser/mytemplate.json --region 'us-east-1' --parameters ParameterKey=Parm1,ParameterValue=test1 ParameterKey=Parm2,ParameterValue=test2"
         //sh "aws cloudformation create-stack --stack-name sundayWebStack --template-file  $workspace/cloudformation/sunday_web_stack.template --region 'us-east-1' --parameters ParameterKey=InstanceType,ParameterValue=t2.micro ParameterKey=KeyName,ParameterValue='s3://cf-templates-a1jqt245fpux-us-east-1/DevOpsKeyPair.pem' ParameterKey=SSHLocation,ParameterValue=0.0.0.0/0"
         //sh "aws cloudformation validate-template --region 'us-east-1' --template-file  $workspace/cloudformation/sunday_web_stack.json" 
-        sh "aws cloudformation deploy --template-file  '$workspace/cloudformation/TrainingEvent-UbuntuServer.json' --stack-name TomCatWebStack --region 'us-east-1' --parameter-overrides InstanceType=t2.micro KeyName='DevOpsKeyPair' SSHLocation=0.0.0.0/0"
-        //sh "echo SKIPPING INFRASTRUCTURE CREATION/UPDATE for now .."
+        //sh "aws cloudformation deploy --template-file  '$workspace/cloudformation/TrainingEvent-UbuntuServer.json' --stack-name TomCatWebStack --region 'us-east-1' --parameter-overrides InstanceType=t2.micro KeyName='DevOpsKeyPair' SSHLocation=0.0.0.0/0"
+        sh "echo SKIPPING INFRASTRUCTURE CREATION/UPDATE for now .."
       }
     }
     stage('Configure Tomcat') {
