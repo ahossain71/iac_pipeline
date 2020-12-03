df95bb0c90f190750eb8bfa415f65d18430d1cbb
AZFAR HOSSAIN <ahossain@us.ibm.com>
2020-12-03 18:41:27 -0500

    Added a new stage Update Inventory in the pieline 12/3 6:41 pm

--- iac_pipeline.groovy
+++ iac_pipeline.groovy
@@ -11,6 +11,17 @@ pipeline {
         sh "echo SKIPPING INFRASTRUCTURE CREATION/UPDATE for now .."
       }
     }
+    stage('Update Inventory'){
+      steps{
+        catchError(buildResult: 'SUCCESS', stageResult: 'FAILURE') {
+          withCredentials([sshUserPrivateKey(credentialsId: 'cff1d3fe-236f-43ca-8ff5-5f37ec63422d', keyFileVariable: 'myKEY')]) {
+             sh 'ansible-playbook ./ansible/playbooks/update_inventory.yml --user ubuntu --key-file ${myKEY} -e "root_user=ubuntu hosts_group=tomcat-nodes"'  
+           }//end withCredentials
+          sh "exit 0"
+         }//end catchError
+
+      }
+    }
     stage('Configure Tomcat') {
       steps {
         catchError(buildResult: 'SUCCESS', stageResult: 'FAILURE') {
