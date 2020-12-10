4cc951319b01737b1004d6febada71dfa9fae932
AZFAR HOSSAIN <ahossain@us.ibm.com>
2020-12-10 18:23:43 -0500

    Testing automated infrastructure deloyment 12/10 6:23 pm

--- iac_pipeline.groovy
+++ iac_pipeline.groovy
@@ -11,9 +11,13 @@ pipeline {
     stage('Update Inventory'){
       steps{
         catchError(buildResult: 'SUCCESS', stageResult: 'FAILURE') {
-          withCredentials([sshUserPrivateKey(credentialsId: 'cff1d3fe-236f-43ca-8ff5-5f37ec63422d', keyFileVariable: 'myKEY')]) {
+          //withCredentials([sshUserPrivateKey(credentialsId: 'cff1d3fe-236f-43ca-8ff5-5f37ec63422d', keyFileVariable: 'myKEY')]) {
+          //  sh 'ansible-playbook ./ansible/playbooks/update_inventory.yml --user ubuntu --key-file ${myKEY}'  
+          // }//end withCredentials
+           withCredentials([sshUserPrivateKey(credentialsId: 'DevOpsKeyPair', keyFileVariable: 'myKEY')]) {
              sh 'ansible-playbook ./ansible/playbooks/update_inventory.yml --user ubuntu --key-file ${myKEY}'  
            }//end withCredentials
+
           sh "exit 0"
          }//end catchError
 
@@ -22,7 +26,10 @@ pipeline {
     stage('Configure Tomcat') {
       steps {
         catchError(buildResult: 'SUCCESS', stageResult: 'FAILURE') {
-          withCredentials([sshUserPrivateKey(credentialsId: 'cff1d3fe-236f-43ca-8ff5-5f37ec63422d', keyFileVariable: 'myKEY')]) {
+          //withCredentials([sshUserPrivateKey(credentialsId: 'cff1d3fe-236f-43ca-8ff5-5f37ec63422d', keyFileVariable: 'myKEY')]) {
+          //   sh 'ansible-playbook ./ansible/playbooks/tomcat-setup.yml --user ubuntu --key-file ${myKEY}'  
+          // }//end withCredentials
+          withCredentials([sshUserPrivateKey(credentialsId: 'DevOpsKeyPair', keyFileVariable: 'myKEY')]) {
              sh 'ansible-playbook ./ansible/playbooks/tomcat-setup.yml --user ubuntu --key-file ${myKEY}'  
            }//end withCredentials
           sh "exit 0"
