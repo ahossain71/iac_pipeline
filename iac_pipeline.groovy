7bea83c32405ae03b5018ef354555ee29fb52146
AZFAR HOSSAIN <ahossain@us.ibm.com>
2020-12-10 22:59:27 -0500

    Testing automated infrastructure deloyment 12/10 10:59 pm

--- iac_pipeline.groovy
+++ iac_pipeline.groovy
@@ -4,7 +4,7 @@ pipeline {
   stages {
     stage('Submit Stack') { 
       steps {
-        sh "aws cloudformation deploy --template-file  '$workspace/cloudformation/TrainingEvent-UbuntuServer.json' --stack-name TomCatWeb-Stack-Val --region 'us-east-1' --parameter-overrides InstanceType=t2.micro KeyName='2be93d5b-70f9-4db2-adc3-f307b7b86c48' SSHLocation=0.0.0.0/0"
+        sh "aws cloudformation deploy --template-file  '$workspace/cloudformation/TrainingEvent-UbuntuServer.json' --stack-name TomCatWeb-Stack-Val --region 'us-east-1' --parameter-overrides InstanceType=t2.micro KeyName='DevOpsKeyPair' SSHLocation=0.0.0.0/0"
         //sh "echo SKIPPING INFRASTRUCTURE CREATION/UPDATE for now .."
       }
     }
