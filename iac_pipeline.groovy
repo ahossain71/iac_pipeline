4e54af5a6517f844d2fc34f2b1a79eea8a1220e8
ahossain71 <azfar.hossain@yahoo.comgit config --global user.email azfar.hossain@yahoo.comgit config --global user.email azfar.hossain@yahoo.com>
2021-02-06 14:41:01 -0500

    deploying after configuring Jenkins server for aws cli commands 020602240

--- iac_pipeline.groovy
+++ iac_pipeline.groovy
@@ -5,7 +5,7 @@ pipeline {
     stage('Submit Stack') { 
       steps {
           catchError(buildResult: 'SUCCESS', stageResult: 'FAILURE') {
-            withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: 'aws-key', usernameVariable: 'AWS_ACCESS_KEY_IDAKIA4N7WPXTNEXPLQEJK', passwordVariable: 'TYG5GX9Xleki29xMZsc1nybFp7uzoPo/in4siDPN']]) {
+            withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: 'aws-key', usernameVariable: 'AKIA4N7WPXTNEXPLQEJK', passwordVariable: 'TYG5GX9Xleki29xMZsc1nybFp7uzoPo/in4siDPN']]) {
               //sh "aws cloudformation deploy --template-file  '$workspace/cloudformation/TrainingEvent-UbuntuServer.json' --stack-name TomCatWeb-Stack-Val --region 'us-east-1' --parameter-overrides InstanceType=t2.micro KeyName='myTestKeyPair02' SSHLocation=0.0.0.0/0"
               sh "aws s3 ls"
               //sh "echo SKIPPING INFRASTRUCTURE CREATION/UPDATE for now .."
