c9573ef806627b7b09caf1d1c9be6a5459777188
ahossain71 <azfar.hossain@yahoo.comgit config --global user.email azfar.hossain@yahoo.comgit config --global user.email azfar.hossain@yahoo.com>
2021-02-13 23:22:26 -0500

    modifield iac_pilene source for dynamic inventory confoguration 02131122

--- iac_pipeline.groovy
+++ iac_pipeline.groovy
@@ -19,7 +19,8 @@ pipeline {
         catchError(buildResult: 'SUCCESS', stageResult: 'FAILURE') {
            //sh "echo SKIPPING Automated update of inventory file in lieu to manual update .."
              withCredentials([sshUserPrivateKey(credentialsId: 'a59a13e3-8e2f-4920-83c9-a49b576e5d58', keyFileVariable: 'myTestKeyPair02')]) {
-             sh 'ansible-playbook ./ansible/playbooks/update_inventory.yml --user ubuntu --key-file ${myTestKeyPair02}'  
+            // sh 'ansible-playbook ./ansible/playbooks/update_inventory.yml --user ubuntu --key-file ${myTestKeyPair02}'  
+              sh '/etc/ansible/ec2.py --list'
            }//end withCredentials
           sh "exit 0"
          }//end catchError
@@ -29,7 +30,7 @@ pipeline {
       steps {
         catchError(buildResult: 'SUCCESS', stageResult: 'FAILURE') {
           withCredentials([sshUserPrivateKey(credentialsId: 'a59a13e3-8e2f-4920-83c9-a49b576e5d58', keyFileVariable: 'myKEY')]) {
-             //sh 'ansible-playbook ./ansible/playbooks/tomcat-setup.yml --user ubuntu --key-file ${myKEY}'  
+             sh 'ansible-playbook ./ansible/playbooks/tomcat-setup.yml --user ubuntu --key-file ${myKEY}'  
            }//end withCredentials
           sh "exit 0"
          }//end catchError
