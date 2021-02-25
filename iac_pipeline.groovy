d1ab77540bb2372dfe978ec079ae1345356de219
ahossain71 <azfar.hossain@yahoo.comgit config --global user.email azfar.hossain@yahoo.comgit config --global user.email azfar.hossain@yahoo.com>
2021-02-24 21:50:23 -0500

    reconfiguring dynamic host using local inventory file 0224950

--- iac_pipeline.groovy
+++ iac_pipeline.groovy
@@ -19,8 +19,8 @@ pipeline {
         catchError(buildResult: 'SUCCESS', stageResult: 'FAILURE') {
             //sh "echo SKIPPING Automated update of inventory file in lieu to manual update .."
              withCredentials([sshUserPrivateKey(credentialsId: 'a59a13e3-8e2f-4920-83c9-a49b576e5d58', keyFileVariable: 'myTestKeyPair02')]) {
-                sh 'python /etc/ansible/ec2.py --list'
-                //sh 'ansible-playbook ./ansible/playbooks/update_inventory.yml --user ubuntu --key-file ${myTestKeyPair02}'  
+                //sh 'python /etc/ansible/ec2.py --list'
+                sh 'ansible-playbook ./ansible/playbooks/update_inventory.yml --user ubuntu --key-file ${myTestKeyPair02}'  
             // sh '/etc/ansible/ec2.py --list'
            }//end withCredentials
           sh "exit 0"
