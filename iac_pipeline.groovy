cd225d26e2a1106389fc6447daa758ab95fb09af
ahossain71 <azfar.hossain@yahoo.comgit config --global user.email azfar.hossain@yahoo.comgit config --global user.email azfar.hossain@yahoo.com>
2021-03-09 01:26:50 -0500

    modifield iac_pilene source for dynamic inventory confoguration 0213126

--- iac_pipeline.groovy
+++ iac_pipeline.groovy
@@ -6,7 +6,7 @@ pipeline {
       steps {
           catchError(buildResult: 'SUCCESS', stageResult: 'FAILURE') {
             withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: 'Jenkins-server', usernameVariable: 'AWS_ACCESS_KEY_ID', passwordVariable: 'AWS_SECRET_ACCESS_KEY']]) {
-              sh "aws cloudformation deploy --template-file $workspace/cloudformation/TrainingEvent-UbuntuServer.json --stack-name TomCatWeb-Stack-Test --region us-east-1 --parameter-overrides InstanceType=t2.micro KeyName=myTestKeyPair02 SSHLocation=0.0.0.0/0 --tags name=TomCatWeb-Stack-Test"
+              //sh "aws cloudformation deploy --template-file $workspace/cloudformation/TrainingEvent-UbuntuServer.json --stack-name TomCatWeb-Stack-Test --region us-east-1 --parameter-overrides InstanceType=t2.micro KeyName=myTestKeyPair02 SSHLocation=0.0.0.0/0 --tags name=TomCatWeb-Stack-Test"
               //sh "aws cloudformation create-stack --template-body '$workspace/cloudformation/TrainingEvent-UbuntuServer.py' --stack-name TomCatWeb-Stack-Val --region us-east-1 --parameters ParameterKey=InstanceType,ParameterValue=t2.micro ParameterKey=KeyName,ParameterValue='myTestKeyPair02' ParameterKey=SSHLocation,ParameterValue=0.0.0.0/0"
               sh "echo SKIPPING INFRASTRUCTURE CREATION/UPDATE for now .."
             }//end withCredentials
@@ -31,7 +31,7 @@ pipeline {
       steps {
         catchError(buildResult: 'SUCCESS', stageResult: 'FAILURE') {
           withCredentials([sshUserPrivateKey(credentialsId: 'a59a13e3-8e2f-4920-83c9-a49b576e5d58', keyFileVariable: 'myTestKeyPair02')]) {
-            sh 'ansible-playbook ./ansible/playbooks/tomcat-setup.yml --user ubuntu --key-file myTestKeyPair02'  
+            sh 'ansible-playbook ./ansible/playbooks/tomcat-setup.yml --user ubuntu --private-key ${myTestKeyPair02}'  
            }//end withCredentials
           sh "exit 0"
          }//end catchError
