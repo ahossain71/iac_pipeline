7092880c6b32c3e2801a9b10bbc84bfeb735c01b
ahossain71 <azfar.hossain@yahoo.comgit config --global user.email azfar.hossain@yahoo.comgit config --global user.email azfar.hossain@yahoo.com>
2021-03-10 22:05:53 -0500

    modifield iac_pilene source for dynamic inventory confoguration 03101005

--- iac_pipeline.groovy
+++ iac_pipeline.groovy
@@ -31,7 +31,8 @@ pipeline {
       steps {
         catchError(buildResult: 'SUCCESS', stageResult: 'FAILURE') {
           withCredentials([sshUserPrivateKey(credentialsId: 'a59a13e3-8e2f-4920-83c9-a49b576e5d58', keyFileVariable: 'myTestKeyPair02')]) {
-            sh 'ansible-playbook ./ansible/playbooks/tomcat-setup.yml --user ubuntu -vvv --private-key ~./ssh/authorized_keys'
+            sh 'ansible-playbook ./ansible/playbooks/tomcat-setup.yml --user ubuntu -vvv --key-file ${myTestKeyPair02}'
+            //sh 'ansible-playbook ./ansible/playbooks/tomcat-setup.yml --user ubuntu -vvv --private-key ~/.ssh/authorized_keys'
             //sh 'ansible-playbook ./ansible/playbooks/tomcat-setup.yml --user ubuntu'
             //sh 'ansible -m ping tag_Name_tomcat-node01 --user ubuntu -vvv --key-file ${myTestKeyPair02}'
             }//end withCredentials
